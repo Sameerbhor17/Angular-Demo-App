6fe94cb29f6b3957c32da989c0ffbef451b5ba48
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2026-04-27 13:18:59 +0000

    Angular (Learning Curve) - Scoping CSS Styles to Components and Understanding & Configuring View Encapsulation

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  // encapsulation: ViewEncapsulation.Emulated --- This is default encapsulation behaviour for angular which enables style scoping in DOM
+  encapsulation: ViewEncapsulation.None     // --- This will disable style scoping and enables global styling for input and textarea fields
 })
 export class ControlComponent {
   label = input.required<string>();
