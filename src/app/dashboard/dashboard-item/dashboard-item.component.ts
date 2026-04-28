2ac5b1b84192d934777cac4f0ac83d310ce3ec5a
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2026-04-28 10:59:49 +0000

    Angular (Learning Curve) - Host Elements, @HostListener, and @HostBinding

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, input, Input } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler';
+import { Component, HostBinding, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  host: {
+    class: 'dashboard-item'
+  }
 })
 export class DashboardItemComponent {
   // @Input({required: true}) image!: {
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,8 +7,24 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   // encapsulation: ViewEncapsulation.Emulated --- This is default encapsulation behaviour for angular which enables style scoping in DOM
-  encapsulation: ViewEncapsulation.None     // --- This will disable style scoping and enables global styling for input and textarea fields
+  encapsulation: ViewEncapsulation.None,    // --- This will disable style scoping and enables global styling for input and textarea fields
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   alert("Clicked");
+  // }
+
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    // alert("Clicked");
+    console.log("Clicked");
+    console.log(this.el);
+  }
 }
